2f4d4d9f153f7784d53635ef9dcf7e242ab8068a
David Soares <david.junior@ufpi.edu.br>
2026-06-30 10:12:20 -0300

    sombreamento

---
+++ build/CMakeFiles/headsoccer.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Projeto Computacao Grafica/GAME/src/Audio.cpp" "CMakeFiles/headsoccer.dir/src/Audio.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/Audio.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/GameState.cpp" "CMakeFiles/headsoccer.dir/src/GameState.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/GameState.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/HUD.cpp" "CMakeFiles/headsoccer.dir/src/HUD.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/HUD.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/Input.cpp" "CMakeFiles/headsoccer.dir/src/Input.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/Input.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/Model3D.cpp" "CMakeFiles/headsoccer.dir/src/Model3D.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/Model3D.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/ModeloAnimado.cpp" "CMakeFiles/headsoccer.dir/src/ModeloAnimado.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/ModeloAnimado.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/Physics.cpp" "CMakeFiles/headsoccer.dir/src/Physics.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/Physics.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/Renderer.cpp" "CMakeFiles/headsoccer.dir/src/Renderer.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/Renderer.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/Update.cpp" "CMakeFiles/headsoccer.dir/src/Update.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/Update.cpp.obj.d"
+  "C:/Projeto Computacao Grafica/GAME/src/main.cpp" "CMakeFiles/headsoccer.dir/src/main.cpp.obj" "gcc" "CMakeFiles/headsoccer.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/headsoccer.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/headsoccer.dir/src/Audio.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/Audio.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/GameState.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/GameState.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/HUD.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/HUD.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/Input.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/Input.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/Model3D.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/Model3D.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/ModeloAnimado.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/ModeloAnimado.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/Physics.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/Physics.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/Renderer.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/Renderer.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/Update.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/Update.cpp.obj.d"
+  "CMakeFiles/headsoccer.dir/src/main.cpp.obj"
+  "CMakeFiles/headsoccer.dir/src/main.cpp.obj.d"
+  "headsoccer.exe"
+  "headsoccer.exe.manifest"
+  "headsoccer.pdb"
+  "libheadsoccer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/headsoccer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
